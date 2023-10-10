e21eecad88bcaf02269e76c2c9ce78d12a47861b
lyc8503 <admin@lyc8503.site>
2023-10-10 22:44:51 +0800

    add a missing await

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -3,8 +3,8 @@ import { KVNamespace } from '@cloudflare/workers-types'
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
   const { ONEDRIVE_CF_INDEX_KV } = process.env as unknown as { ONEDRIVE_CF_INDEX_KV: KVNamespace }
 
-  const accessToken = ONEDRIVE_CF_INDEX_KV.get('access_token')
-  const refreshToken = ONEDRIVE_CF_INDEX_KV.get('refresh_token')
+  const accessToken = await ONEDRIVE_CF_INDEX_KV.get('access_token')
+  const refreshToken = await ONEDRIVE_CF_INDEX_KV.get('refresh_token')
 
   return {
     accessToken,
@@ -23,6 +23,6 @@ export async function storeOdAuthTokens({
 }): Promise<void> {
   const { ONEDRIVE_CF_INDEX_KV } = process.env as unknown as { ONEDRIVE_CF_INDEX_KV: KVNamespace }
 
-  ONEDRIVE_CF_INDEX_KV.put('access_token', accessToken, { expirationTtl: accessTokenExpiry })
-  ONEDRIVE_CF_INDEX_KV.put('refresh_token', refreshToken)
+  await ONEDRIVE_CF_INDEX_KV.put('access_token', accessToken, { expirationTtl: accessTokenExpiry })
+  await ONEDRIVE_CF_INDEX_KV.put('refresh_token', refreshToken)
 }
