8cc2a8c0f09c5b28b707bda65971568d50e628b4
lyc8503 <me@lyc8503.site>
2023-10-22 00:51:47 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,6 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
+  // This is the path relative to `baseDirectory` above, not relative to root in your Onedrive.
   protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
