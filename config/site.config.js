951eef09212b7fab5584391f7ae8f0795384985b
Melvin Mmehibe <melvinmmehibe@gmail.com>
2024-03-19 02:46:34 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/20231129_101015_0001.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Shutter! MovIES+",
+  title: "Shutter! MOVIES+",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: '/',
