ee3b95a4b4348e7ee021b1f9c3c533d5adaab6e6
Ompriymohan <117296848+Ompriymohan@users.noreply.github.com>
2024-03-20 14:56:31 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your worker's environment variable if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'me@lyc8503.site',
+  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'Noisy_boy@1m0ssz.onmicrosoft.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
