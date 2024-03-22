01b30d4cb0418e9aa2baebe882589e47d1949144
Melvin Mmehibe <melvinmmehibe@gmail.com>
2024-03-22 10:20:38 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/MOVIESPLUS/Folder-1'],
+  protectedRoutes: ['/MOVIESPLUS'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:admin@shutter.ng',
