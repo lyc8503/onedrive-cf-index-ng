f47329250b79fbb82fc20669107fb46cf2a18d86
rentianyu <36915481+rentianyu@users.noreply.github.com>
2025-12-18 00:03:15 +0800

    auto-push

--- config/api.config.js
+++ config/api.config.js
@@ -12,8 +12,8 @@ module.exports = {
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
   // clientId: 'f23f9918-14a5-46f1-9f74-13e57273b8f3',
   // obfuscatedClientSecret: 'U2FsdGVkX1/hHUr0qvtP2dAJuY9MqdXfryBEQW3FkN4AI6MGmoxTOxEANDejnriD/4nJXcg4Lv4sSXI0O5Lf4w==',
-  clientId: process.env.clientId,
-  obfuscatedClientSecret: process.env.obfuscatedClientSecret,
+  clientId: 'c9c5b1b0-edc2-4286-b492-41d2a6a44b93',
+  obfuscatedClientSecret: 'U2FsdGVkX1+IbR1EOUNELZHOr1qbYLlu3H0cFZ4DSgQ0/A1e12rau3n0/Q8YHg+BQESjM9iwlj+uXgCTBJ4lAg==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "lyc8503's OneDrive",
+  title: "小贝塔网盘",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: process.env.BASE_DIRECTORY || '/',
@@ -50,7 +50,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/lyc8503',
+      link: 'https://github.com/rentianyu',
     },
   ],
 
